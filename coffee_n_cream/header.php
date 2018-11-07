0eecea0f8d5e0fc2fde0d647e56e5361feedf7c4
ejmcreates <44686503+ejmcreates@users.noreply.github.com>
2018-11-07 12:43:18 -0800

    Initial website files

---
+++ coffee_n_cream/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
+<meta name="description" content="description"/>
+<meta name="keywords" content="keywords"/> 
+<meta name="author" content="author"/> 
+<link rel="stylesheet" type="text/css" href="default.css" media="screen"/>
+<title>Coffee N Cream</title>
+</head>
+
+<body>
+
+<div class="container">
+
+	<div class="header">
+				
+		<div class="title">
+			<h1>Coffee N Cream</h1>
+		</div>
+
+	</div>
+
+	<div class="navigation">
+			<a href="index.php">HOME</a>
+			<a href="order.php">ORDER</a>
+			<a href="contact.php">CONTACT USWelcome to my </a>
+			<a href="index.html">Maecenas</a>
+			<a href="index.html">Sodales</a>
+			<div class="clearer"><span></span></div>
+		</div>
+
+	<div class="main">		
+		
+		<div class="content">
+            
+            <!--header ends here-->
---
+++ coffee_n_cream/index.php
@@ -0,0 +1,11 @@
+<?php include 'header.php';?>
+
+<h1>Come and get Caffeinated!!</h1>
+
+<p>Espresso dripper chicory that, spoon, a turkish, breve, grounds, body, aroma, viennese extraction, black beans white trifecta decaffeinated saucer cortado sweet americano skinny organic. Doppio skinny, black, cinnamon crema, eu aged, con panna sit, crema, lungo, seasonal macchiato steamed viennese coffee. Coffee, chicory milk, caffeine blue mountain, breve id macchiato ut rich a, cappuccino espresso ristretto americano coffee white whipped shop white. </p>
+
+<p><a href="order.php">Place an Order</a></p>
+<p><a href="contact.php">Contact Us!</a></p>
+        
+
+<?php include 'footer.php';?>
